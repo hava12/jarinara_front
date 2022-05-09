df82fd155bd970b45b1af1480bcd3370c1a1d2cb
dongyeop <hava12@naver.com>
2022-05-10 00:45:27 +0900

    material ui 적용

--- src/App.js
+++ src/App.js
@@ -4,24 +4,43 @@ import Button from '@mui/material/Button';
 import React from 'react'
 import routes from "./routes";
 import Todo from './components/Todo'
+import { Paper, List } from '@mui/material';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Todo />
-        <div className="container">
-            <Routes>
-                <>
-                    {routes.map((route) => {
-                          return <Route path={route.path} key={route.path} element={<route.component />} exact></Route>;
-                    })}
-                </>
-            </Routes>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [{id: 0, title: "Hello World 1", done: true},
+              {id: 1, title: "Hello World 2", done: false}]
+    }
+  }
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{"margin": "16"}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
+    return (
+      <Router>
+        <div className="App">
+          {todoItems}
+          <div className="container">
+              <Routes>
+                  <>
+                      {routes.map((route) => {
+                            return <Route path={route.path} key={route.path} element={<route.component />} exact></Route>;
+                      })}
+                  </>
+              </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { ListItem, Checkbox, ListItemText, InputBase } from '@mui/material';
 
-function Todo() {
-  return (
-      <div className="Todo">
-          <input type='checkbox' id='todo0' name='todo0' value='todo0' />
-          <label for="todo0">Todo 컴포넌트 만들기</label>
-      </div>
-  );
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {item: props.item}
+    }
+    render() {  
+        const item = this.state.item;
+        return (
+            <ListItem>
+                <Checkbox checked={item.done} />
+                <ListItemText>
+                    <InputBase
+                        inputProps={{"aria-label": "naked"}}
+                        type="text"
+                        id={item.id}
+                        name={item.id}
+                        value={item.title}
+                        multiline={true}
+                        fullWidth={true}
+                    />
+                </ListItemText>
+            </ListItem>
+        );  
+    }
 }
 
 export default Todo;
