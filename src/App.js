798f20b511b9fc3c04c84d12edd759ca8fee8059
dongyeop <hava12@naver.com>
2022-05-10 00:01:56 +0900

    프론트앤드 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import LoginForm from './components/main/LoginForm.js';
 import './App.css';
-import Header from './components/layout/Header.js';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Button from '@mui/material/Button';
+import React from 'react'
 import routes from "./routes";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-          <div className="container">
-              <Routes>
-                  <>
-                      {routes.map((route) => {
-                            return <Route path={route.path} key={route.path} element={<route.component />} exact></Route>;
-                      })}
-                  </>
-              </Routes>
-          </div>
+        <Button>Primary</Button>
+        <Button disabled>Disabled</Button>
+        <Button href="#text-buttons">Link</Button>
+        <div className="container">
+            <Routes>
+                <>
+                    {routes.map((route) => {
+                          return <Route path={route.path} key={route.path} element={<route.component />} exact></Route>;
+                    })}
+                </>
+            </Routes>
+        </div>
       </div>
     </Router>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,4 @@
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import User from "./pages/User";
-import Users from "./pages/Users";
-import LoginForm from "./components/main/LoginForm";
 
 export default [
-    {
-        path: "/",
-        component: Home,
-        needsAuth: false
-    },
-    {
-        path: "/movies",
-        component: Movies,
-        needsAuth: true
-    },
-    {
-        path: "/users",
-        component: Users,
-        needsAuth: true
-    },
-    {
-        path: "/users/:id",
-        component: User,
-        needsAuth: true
-    },
-    {
-        path: "/login",
-        component: LoginForm,
-        needsAuth: true
-    }
+    
 ];
